41b0cc9116b43854d943ced52aa4f6c88cf0615c
abraham <abrahamikpoma@gmail.com>
2022-12-08 11:37:36 -0800

    thee

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 /**
- * Main -Entry
- * Return: Alway 0
+ * Main- Entry
+ * Return: Alway 0 (Success)
+ *
  *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 
+
+
 }
+
