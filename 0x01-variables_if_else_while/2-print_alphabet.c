846135018dfd7a3bc6a1c0b118216eab1ea29b3c
abraham <abrahamikpoma@gmail.com>
2022-12-12 20:31:55 -0800

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+char c = 'a';
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+
+putchar('\n');
+return (0);
+}
