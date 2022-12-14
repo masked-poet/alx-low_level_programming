0dd7f2a25f318ebd09040740b4fbdb2dbc6e4b9d
abraham <abrahamikpoma@gmail.com>
2022-12-13 21:35:37 -0800

    task all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,8 @@
 */
 int main(void)
 {
-/**	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
-	int count, sz;
-
+	int str[] = {95, 112, 117, 116, 		99, 104, 97, 114};
+		int count, sz;
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
@@ -17,16 +15,4 @@ int main(void)
 
 	} _putchar('\n');
 	return (0);
-
-*/
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-
-}
+					}
