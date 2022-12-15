61e48603623f2f0c41d1b08876c35eeadea6527b
abraham <abrahamikpoma@gmail.com>
2022-12-15 13:40:01 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,20 +2,16 @@
 
 /**
  * print_line - entry point
- *
- *
  * Description: Prints lines
  *@n: number of lines
- *
  * Return: void
- *
  */
 void print_line(int n)
 {
 	int num_lines;
-	if (n <= 0)
 
-{
+	if (n <= 0)
+	{
 		_putchar('\n');
 	}
 	else
