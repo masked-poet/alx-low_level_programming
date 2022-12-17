cff42df96537242d4f825350e3532d7ad828e5bb
abraham <abrahamikpoma@gmail.com>
2022-12-17 01:04:27 -0800

    task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *
+ *Return: valve 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (a = 0; a < 10; b++)
+	{
+		putchar(a + '0');
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+
+}
