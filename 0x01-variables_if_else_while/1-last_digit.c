df93ab88dd4bf66d1876ff9b5db1cb82ef0b4981
abraham <abrahamikpoma@gmail.com>
2022-12-16 23:30:19 -0800

    redo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,15 @@
 #include <time.h>
 
 /**
- *  *main - function
+ *main - function
  *
- *   *Return: valve 0
+ *Return: valve 0
  *
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
@@ -24,8 +25,6 @@ int main(void)
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
-	}
-	
-return (0};
 }
-
+return (0);
+			}
