3d26c09e856677836b14827ad5129fd7879ad360
abraham <abrahamikpoma@gmail.com>
2022-12-19 21:00:24 -0800

    redo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,9 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-
 		i++;
-	for (i = i - 1; 1 >= i--)
-	_putchar(str[i]);
-
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
