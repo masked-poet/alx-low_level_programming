627ca2003250a9cf6888684d567d071f6e6618f3
abraham <abrahamikpoma@gmail.com>
2023-01-04 09:43:52 -0800

    redo

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,17 +5,16 @@
  *
  *@s: The string.
  *Return: length
+ *
  */
-
 int _strlen_recursion(char *s)
 {
 	int length = 0;
 
 	if (*s == 0)
-
-		if (*s == 0)
-
-		length = _strlen_recursion(s + 1);
+		return (0);
+	length = _strlen_recursion(s + 1);
 
 	return (length + 1);
 }
+
