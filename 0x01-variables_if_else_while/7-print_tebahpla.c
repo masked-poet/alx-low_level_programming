c779087bd5e461d3fbdffbd8ad134bf6d7fded78
abraham <abrahamikpoma@gmail.com>
2023-01-06 19:36:48 -0800

    library ...update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  *main - function
  *
  *Return: valve 0
- *
  */
 int main(void)
 {
-	char c:
-		for (c = 'z'; c >= 'a'; c--)
-
-		{
-			putchar(c);
-
-		}
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
+
