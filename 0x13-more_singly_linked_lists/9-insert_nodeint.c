a867745e914b76396017833ffb1d8ce8a22d1094
abraham <abrahamikpoma@gmail.com>
2023-02-01 00:39:26 -0800

    redo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,6 @@
  *
  * Return: Address of the new node, or NULL if it failed
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *tmp_node = *head;
@@ -21,26 +20,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp_node = tmp_node->next;
 		i++;
 	}
-	if (tmp_node || (i == idx - 1 || idx == 0)
-			{
+	if (tmp_node || (i == idx - 1 || idx == 0))
+	{
 		new_node = malloc(sizeof(listint_t));
 		if (new_node)
 		{
 			new_node->n = n;
-			if (idx == 0)
-			{
-				new_node->next = *head;
-				*head = new_node;
-			}
-			else
-			{
-				new_node->next = tmp_node->next;
-				tmp_node->next = new_node;
+		if (idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
 		}
-			return (new_node);
+		else
+		{
+			new_node->next = tmp_node->next;
+			tmp_node->next = new_node;
+		}
+		return (new_node);
 		}
 	}
 	return (NULL);
 }
-
-
